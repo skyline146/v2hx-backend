7a4503a0faf93c54122bc6640939392e04a79b74
Dmitriy Bukatsel <dimasgrand16@gmail.com>
2023-11-06 00:17:34 +0300

    fix: cookies set

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -81,10 +81,14 @@ export class AuthController {
 
     res.cookie("accessToken", user.accessToken, {
       httpOnly: true,
+      secure: false,
+      sameSite: "strict",
       expires: new Date(Date.now() + 60 * 60 * 1000),
     });
     res.cookie("refreshToken", user.refreshToken, {
       httpOnly: true,
+      secure: false,
+      sameSite: "strict",
       expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
     });
 
